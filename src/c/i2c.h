9de581d678b4ba3858b5e379767954d7667a6e90
Tao Tien <29749622+taotien@users.noreply.github.com>
2022-07-03 18:16:35 -0700

    new board

--- src/c/i2c.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-extern "C" void i2c_setup(void (*recv_callback)(int),
-			  void (*req_callback)(void));
-void i2c_receive(void (*callback)(int));
-void i2c_request(void (*callback)(void));
-extern "C" void i2c_read();
-extern "C" void i2c_write(uint8_t *send, int len);
--- src/c/main.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-void i2c_receive_callback(int len);
-void i2c_request_callback();
-extern void i2c_setup(void (*recv_callback)(int), void (*req_callback)(void));
-extern void i2c_read();
-extern void i2c_write(uint8_t *send, unsigned int len);
-void tuh_mount_cb(uint8_t dev_addr);
-void tuh_unmount_cb(uint8_t dev_addr);
